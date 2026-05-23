a37f74f4397464800c97d032d98ec0ffc51607ac
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2026-05-23 11:48:19 +0500

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+logo= "40ef4cf2ee6a72db2a5af55c231192bd.png"
 st.set_page_config(layout="wide")
-st.title("Ashes")
+st.set_page_config(
+  page_title="Ashes",
+  page_icon=logo,
+  layout="wide"
+)
+
+st.sidebar.image(logo, use_container_width=True)
+
 st.write("Discord Bot For Ages")
 name = st.text_input("Enter your name")
 if name:
