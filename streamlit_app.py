e86b1266ff7e5fa0f44b035a93b5bd5222a5169f
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2026-05-23 11:42:03 +0500

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(layout="wide")
-st.title("My First App")
-st.write("Hello, Streamlit!")
+st.title("Ashes")
+st.write("Discord Bot For Ages")
 name = st.text_input("Enter your name")
 if name:
   st.success(f"Welcome, {name}!")
