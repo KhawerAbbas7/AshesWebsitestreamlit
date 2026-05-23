739c2a41060b2da3cfee330e4c040579f4a44ff2
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2026-05-23 12:32:47 +0500

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests 
 logo= "40ef4cf2ee6a72db2a5af55c231192bd.png"
 st.set_page_config(layout="wide")
 st.set_page_config(
@@ -7,15 +8,14 @@ st.set_page_config(
   layout="wide"
 )
 col1, col2 = st.columns([1, 8])
-
 with col1:
   st.image(logo, width=60)
-
 with col2:
   st.markdown("## Ashes")
-#st.sidebar.image(logo, use_container_width=True)
-#st.title("Ashes")
+
 st.write("Discord Bot For Ages")
+data = requests.get("https://api.codetabs.com/v1/proxy?quest=http://51.75.118.79:20375/matches/getrecent")
+st.write(data)
 name = st.text_input("Enter your name")
 if name:
   st.success(f"Welcome, {name}!")
