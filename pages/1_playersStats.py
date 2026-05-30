5720ed41f4efcfd4e4b57f1898984dfc8f22575e
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2026-05-30 14:20:22 +0500

    Delete pages directory

--- pages/1_playersStats.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import requests
-import streamlit.components.v1 as components
-logo = "40ef4cf2ee6a72db2a5af55c231192bd.png"
-BASE = "http://51.75.118.79:20375"
-st.set_page_config(page_title="Ashes", page_icon=logo, layout="wide")
